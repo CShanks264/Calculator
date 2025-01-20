702e8dca5ec1d87ffd655d97729fff30bba7fb6e
CShanks <cshanks264@gmail.com>
2025-01-20 02:53:47 -0500

    added classes to the buttons directly and light styles

---
+++ main.js
@@ -0,0 +1,9 @@
+// const button1 = document.getElementsById('1')
+
+// // Function that takes the name of the button and inputs it into text box
+// const buttonToText = (button) => {
+//     if (button === querySelector(button)){
+
+//     }
+// }
+
