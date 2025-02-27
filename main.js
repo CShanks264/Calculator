ea2361ce3f8f50c64136025d3e0fa0186aa1e754
CShanks <cshanks264@gmail.com>
2025-02-27 03:22:27 -0500

    Made some js functions and got display() & clearDisplay() functional

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
-//Testing out a event listener that console logs word when 1 is pressed
-const oneButton = document.getElementById("1")
+/* I was trying to get the calculator to work with event listeners
+but I couldn't get it to work */
 
-oneButton.addEventListener("click", e => {
-    console.log('Word')
-})
+
+// const numButtons = document.getElementsByClassName('buttons')
+// const opButtons = document.getElementsByClassName('opButtons')
+// const equals = document.getElementById('equals')
+ 
+// numButtons.addEventListener('click', e => {
+//     textValue = e.value
+// })
+
+// equals.addEventListener('click', (textValue) => {
+//     if (textValue === Number || textValue === opButtons){
+        
+//     } else {
+//         textValue += 'ERROR NaN'
+//     }
+// })
+
+
+const textBox = document.getElementById('textBox')
+const textValue = textBox.value
+
+
+// Function that will display the button pressed
+const display = input => {
+    textBox.value += input
+}
+
+
+//Function that will clear the textBox
+const clearDisplay = input => {
+    textBox.value = ''
+}
+
+// Function that will act as a backspace/delete key
+const backspace = input => {
+
+}
+
+//Function that Solves the given equation
+const solve = input => {
+
+}
